e55ee4d87c7a9e20bbabe944ede837c49c0ad50c
carlosMartinx04 <carloscokchi04@gmail.com>
2025-10-04 03:47:32 +0200

    Continuacion TabMother
    Parte1

--- src/main/java/com/example/javafxtest/HelloApplication.java
+++ src/main/java/com/example/javafxtest/HelloApplication.java
@@ -3,15 +3,44 @@ package com.example.javafxtest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Stack;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        TabPane tabMother = new TabPane();
+
+        Pane pane = new Pane();
+        Button buttonPane = new Button("Absoluto");
+        buttonPane.setLayoutX(50);buttonPane.setLayoutY(50);
+        pane.getChildren().add(buttonPane);//Añadimos el Boton.
+        Tab tabPane = new Tab("Pane");
+        tabPane.setContent(pane);//Añado el Pane al contenido del TabPane.
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        StackPane stackPane = new StackPane();
+        Button buttonStackPane = new Button("Centrado");
+        stackPane.getChildren().add(buttonStackPane);
+        Tab tabStackPane = new Tab("StackPane");
+        tabStackPane.setContent(stackPane);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        HBox hBox = new HBox();
+        Button A = new Button("A");
+        Button B = new Button("B");
+        Button C = new Button("C");
+
+
+        tabMother.getTabs().addAll(tabPane, tabStackPane);//Añado el TabPane al TabMother para que forme parte de el.
+        Scene scene = new Scene(tabMother, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
